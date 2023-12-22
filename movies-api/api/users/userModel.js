14c9f8486b0ea6113e32a0d3339df66cdc4a08b1
AndrewK0val <andrewkoval2003@gmail.com>
2023-12-22 23:49:37 +0000

    added API from labs and movies app from previous CA

---
+++ movies-api/api/users/userModel.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true, required: true},
+  password: {
+    type: String, 
+    match: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, 
+    message: 'Password must be at least 8 characters long and include at least one letter, one digit, and one special character (@$!%*#?&)', required: true 
+  }
+});
+
+UserSchema.methods.comparePassword = async function (passw) { 
+  return await bcrypt.compare(passw, this.password); 
+}
+
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
+
+  } else {
+      next();
+  }
+});
+
+export default mongoose.model('User', UserSchema);
